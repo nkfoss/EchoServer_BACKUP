0f2467815fb7a5c6bb53dce18c6bf87c58a02d43
nkfoss <nathankfoss@gmail.com>
2019-10-16 15:54:33 -0500

    Flush system.out and add comments narrating the code
    The reason system.out.flush() is used is so that all the binary data
    gets outputted before the connection is finished. Without this, a chunk
    of data is lost at the end of the stream.
    
    (this is the reason why we might get a gray square in the lower
    right-hand corner of our output image... we lost the pixels.)

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -10,37 +10,52 @@ public class EchoClient {
 
     public static void main(String[] args) {
 
-        String server = "127.0.0.1";
+        String server;
 
         // Use "127.0.0.1", i.e., localhost, if no server is specified.
-//        if (args.length == 0) {
-//            server = "127.0.0.1";
-//        } else {
-//            server = args[0];
-//        }
+        if (args.length == 0) {
+            server = "127.0.0.1";
+        } else {
+            server = args[0];
+        }
 
+        // Be prepared to handle any general exception
         try {
+            // Establish connection with the server on the specified portnumber
             Socket echoSocket = new Socket(server, portNumber);
+
+            // Grab the input/output streams so we can write/read directly to/from them
             OutputStream os = echoSocket.getOutputStream();
             DataInputStream is = new DataInputStream(echoSocket.getInputStream());
 
+            // We are communicating via bytes. Characters entered into the console are translated to bytes
+            // and sent to the server, which interprets them as bytes.
             int c;
             int responseByte;
 
+            // While there is a valid input to be received from System.in...
             while ( (c = System.in.read()) != -1) {
+
+                // Write it and send it all together, immediately
                 os.write(c);
                 os.flush();
+
+                // Read the response and response and output it all immediately.
                 responseByte = is.read();
                 System.out.write(responseByte);
+                System.out.flush();
             }
 
+            // Feedback for the user. This is commented out so the tests pass.
 //            System.out.println("Time to close.");
 
+            // Close everything when we are done.
             os.close();
             is.close();
             echoSocket.close();
         }
 
+        // Very minimal error handling.
         catch (Exception e) {
             System.err.println("Exception:  " + e);
         }
