bfdbcce5882f33a280d0e2a5533c9f3a902fa691
nkfoss <nathankfoss@gmail.com>
2019-10-16 15:55:24 -0500

    Add comments to narrate the code

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -15,36 +15,39 @@ public class EchoServer {
             // Start listening on the specified port
             ServerSocket serverSocket = new ServerSocket(portNumber);
 
+            // The client is sending bytes, represented as an int
             int c;
-            String responseLine;
 
             // Run forever, which is common for server style services
             while (true) {
-                // Wait until someone connects, thereby requesting a date
+
+                // Wait until someone connects, and indicate this to the user.
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from client.");
 
-                // Construct a writer so we can write to the socket, thereby
-                // sending something back to the client.
+                // Grab the input/output streams so we can write/read directly to/from them
                 OutputStream os = clientSocket.getOutputStream();
                 DataInputStream is = new DataInputStream(clientSocket.getInputStream());
 
-                // c = is.read();
+                // While there is valid input to be read...
                 while ( (c = is.read()) != -1) {
+
+                    // Write that input back into the output stream, and send it all immediately.
                     os.write(c);
                     os.flush();
                 }
 
-                // Close the client socket since we're done.
+                // Close the streams/client socket since we're done.
                 System.out.println("Closing client connection");
                 os.close();
                 is.close();
                 clientSocket.close();
-
             }
 
-            // *Very* minimal error handling.
-        } catch (IOException ioe) {
+        }
+
+        // Very minimal error handling.
+        catch (IOException ioe) {
             System.out.println("We caught an unexpected exception");
             System.err.println(ioe);
         }
